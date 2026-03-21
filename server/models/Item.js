f8d5e5b9ec4bfddd0f4fdf7b72db7dca89fdf63e
Adithya Kavin Sankar <z5481981@ad.unsw.edu.au>
2026-03-21 11:11:37 +1100

    helper functions for all apis

--- server/models/Item.js
+++ server/models/Item.js
@@ -11,4 +11,44 @@ const createItem = async ({ seller_id, title, description, starting_price, end_t
     return result.rows[0];
 };
 
-module.exports = { createItem };
+const findItemById = async (id) => {
+    const result = await db.query('SELECT * FROM items WHERE id = $1', [id]);
+    return result.rows[0];
+}
+
+const cancelItem = async (id, seller_id) => {
+    const query = `
+        UPDATE items 
+        SET status = 'canceled' 
+        WHERE id = $1 AND seller_id = $2 
+        RETURNING *;
+    `;
+    const result = await db.query(query, [id, seller_id]);
+    return result.rows[0];
+}
+
+// Get items for the "Marketplace" (Active and NOT mine)
+const getBuyableItems = async (userId) => {
+    const query = `
+        SELECT * FROM items 
+        WHERE seller_id != $1 AND status = 'active' AND end_time > NOW()
+        ORDER BY created_at DESC;
+    `;
+    const result = await db.query(query, [userId]);
+    return result.rows;
+}
+
+// Get items I am selling
+const getListingItems = async (userId) => {
+    const query = 'SELECT * FROM items WHERE seller_id = $1 ORDER BY created_at DESC;';
+    const result = await db.query(query, [userId]);
+    return result.rows;
+}
+
+const getAllItems = async () => {
+    const query = 'SELECT * FROM items';
+    const result = await db.query(query);
+    return result.rows;
+}
+
+module.exports = { createItem, cancelItem, getBuyableItems, getListingItems, findItemById, getAllItems }
